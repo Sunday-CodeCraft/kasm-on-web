6ff83d717bee5405bd093c39d1c3ae320e06c205
Connor Shubham Verlekar <63520509+connorv001@users.noreply.github.com>
2023-01-22 03:34:56 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 
-sudo dd if=/dev/zero bs=1M count=1024 of=/mnt/1GiB.swap
+sudo dd if=/dev/zero bs=1M count=1024 of=/mnt/2GiB.swap
 
-sudo chmod 600 /mnt/1GiB.swap
+sudo chmod 600 /mnt/2GiB.swap
 
-sudo mkswap /mnt/1GiB.swap
+sudo mkswap /mnt/2GiB.swap
 
-sudo swapon /mnt/1GiB.swap
+sudo swapon /mnt/2GiB.swap
 
 
 #Verify swap exists
@@ -15,7 +15,7 @@ cat /proc/swaps
 
 #Add swap to fstab
 
-echo '/mnt/1GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
+echo '/mnt/2GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
 
 
 set -e
